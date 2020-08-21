0f96c4ebf96f7c19e923b839f22ae97fcf97dc10
huutai <truongtai137@gmail.com>
2020-08-22 04:37:15 +0700

    convert to tabs

--- outline_renderer.py
+++ outline_renderer.py
@@ -16,8 +16,8 @@ in vec3 vertex_normal;
 
 void main()
 {
-    vec3 vertex = (matrix_world * vec4(position, 1)).xyz + vertex_normal * 0.1 * outline_size;
-    gl_Position = perspective_matrix * vec4(vertex, 1);
+	vec3 vertex = (matrix_world * vec4(position, 1)).xyz + vertex_normal * 0.1 * outline_size;
+	gl_Position = perspective_matrix * vec4(vertex, 1);
 }
 """
 
@@ -26,100 +26,100 @@ uniform vec4 color;
 
 void main()
 {
-    gl_FragColor = color;
+	gl_FragColor = color;
 }
 """
 
 def get_mesh(object):
-    depsgraph = bpy.context.evaluated_depsgraph_get()
-    bm = bmesh.new()
-    mesh_to_render = bpy.data.meshes.new("AAAAAAAAAAAAAAAA")
-    mesh_to_render.create_normals_split()
-    
-    bm.from_object(object, depsgraph, cage=False, face_normals=False)
-    bm.to_mesh(mesh_to_render)
-    bm.free()
-    
-    return mesh_to_render
+	depsgraph = bpy.context.evaluated_depsgraph_get()
+	bm = bmesh.new()
+	mesh_to_render = bpy.data.meshes.new("AAAAAAAAAAAAAAAA")
+	mesh_to_render.create_normals_split()
+
+	bm.from_object(object, depsgraph, cage=False, face_normals=False)
+	bm.to_mesh(mesh_to_render)
+	bm.free()
+
+	return mesh_to_render
 
 def draw_mesh(scene):
-    draw_handlers = scene.custom_shaders_draw_handlers
-    for draw_handler in draw_handlers:
-        try:
-            bpy.types.SpaceView3D.draw_handler_remove(draw_handler, "WINDOW")
-        except (AttributeError, ValueError) as e:
-            pass
-        draw_handlers.remove(draw_handler)
-    
-    if bpy.context.active_object.type != "MESH" or not scene.use_outline_shaders:
-        return
+	draw_handlers = scene.custom_shaders_draw_handlers
+	for draw_handler in draw_handlers:
+		try:
+			bpy.types.SpaceView3D.draw_handler_remove(draw_handler, "WINDOW")
+		except (AttributeError, ValueError) as e:
+			pass
+		draw_handlers.remove(draw_handler)
 
-    mesh = get_mesh(bpy.context.active_object)
-    mesh.calc_loop_triangles()
+	if bpy.context.active_object.type != "MESH" or not scene.use_outline_shaders:
+		return
 
-    vertices = np.empty((len(mesh.vertices), 3), 'f')
-    normals = np.empty((len(mesh.vertices), 3), 'f')
-    indices = np.empty((len(mesh.loop_triangles), 3), 'i')
+	mesh = get_mesh(bpy.context.active_object)
+	mesh.calc_loop_triangles()
 
-    mesh.vertices.foreach_get(
-        "co", np.reshape(vertices, len(mesh.vertices) * 3))
-    mesh.vertices.foreach_get(
-        "normal", np.reshape(normals, len(mesh.vertices) * 3))
-    mesh.loop_triangles.foreach_get(
-        "vertices", np.reshape(indices, len(mesh.loop_triangles) * 3))
+	vertices = np.empty((len(mesh.vertices), 3), 'f')
+	normals = np.empty((len(mesh.vertices), 3), 'f')
+	indices = np.empty((len(mesh.loop_triangles), 3), 'i')
 
-    shader = gpu.types.GPUShader(vertex_shader, pixel_shader)
-    fmt = gpu.types.GPUVertFormat()
-    fmt.attr_add(id="position", comp_type="F32", len=3, fetch_mode="FLOAT")
-    fmt.attr_add(id="vertex_normal", comp_type="F32", len=3, fetch_mode="FLOAT")
-    vbo = gpu.types.GPUVertBuf(len=len(mesh.vertices), format=fmt)
-    vbo.attr_fill(id="position", data=vertices)
-    vbo.attr_fill(id="vertex_normal", data=normals)
-    ibo = gpu.types.GPUIndexBuf(type="TRIS", seq=indices)
-    batch = gpu.types.GPUBatch(type="TRIS", buf=vbo, elem=ibo)
+	mesh.vertices.foreach_get(
+		"co", np.reshape(vertices, len(mesh.vertices) * 3))
+	mesh.vertices.foreach_get(
+		"normal", np.reshape(normals, len(mesh.vertices) * 3))
+	mesh.loop_triangles.foreach_get(
+		"vertices", np.reshape(indices, len(mesh.loop_triangles) * 3))
 
-    def draw():
-        shader.bind()
-        shader.uniform_float("outline_size", scene.custom_outline_size)
-        shader.uniform_float("color", (0, 0, 0, 1))
-        shader.uniform_float("perspective_matrix", bpy.context.region_data.perspective_matrix)
-        shader.uniform_float("matrix_world", bpy.context.active_object.matrix_world)
-        bgl.glEnable(bgl.GL_CULL_FACE)
-        bgl.glCullFace(bgl.GL_FRONT)
-        bgl.glEnable(bgl.GL_DEPTH_TEST)
-        batch.draw(shader)
-        bgl.glDisable(bgl.GL_CULL_FACE)
-        bgl.glCullFace(bgl.GL_BACK)
-        bgl.glDisable(bgl.GL_DEPTH_TEST)
-    
-    draw_handlers.append(bpy.types.SpaceView3D.draw_handler_add(draw, (), 'WINDOW', 'POST_VIEW'))
-    
-    bpy.data.meshes.remove(mesh)
+	shader = gpu.types.GPUShader(vertex_shader, pixel_shader)
+	fmt = gpu.types.GPUVertFormat()
+	fmt.attr_add(id="position", comp_type="F32", len=3, fetch_mode="FLOAT")
+	fmt.attr_add(id="vertex_normal", comp_type="F32", len=3, fetch_mode="FLOAT")
+	vbo = gpu.types.GPUVertBuf(len=len(mesh.vertices), format=fmt)
+	vbo.attr_fill(id="position", data=vertices)
+	vbo.attr_fill(id="vertex_normal", data=normals)
+	ibo = gpu.types.GPUIndexBuf(type="TRIS", seq=indices)
+	batch = gpu.types.GPUBatch(type="TRIS", buf=vbo, elem=ibo)
+
+	def draw():
+		shader.bind()
+		shader.uniform_float("outline_size", scene.custom_outline_size)
+		shader.uniform_float("color", (0, 0, 0, 1))
+		shader.uniform_float("perspective_matrix", bpy.context.region_data.perspective_matrix)
+		shader.uniform_float("matrix_world", bpy.context.active_object.matrix_world)
+		bgl.glEnable(bgl.GL_CULL_FACE)
+		bgl.glCullFace(bgl.GL_FRONT)
+		bgl.glEnable(bgl.GL_DEPTH_TEST)
+		batch.draw(shader)
+		bgl.glDisable(bgl.GL_CULL_FACE)
+		bgl.glCullFace(bgl.GL_BACK)
+		bgl.glDisable(bgl.GL_DEPTH_TEST)
+
+	draw_handlers.append(bpy.types.SpaceView3D.draw_handler_add(draw, (), 'WINDOW', 'POST_VIEW'))
+
+	bpy.data.meshes.remove(mesh)
 
 class OBJECT_PT_outline_rendering(bpy.types.Panel):
-    bl_label = "Outline Rendering"
-    bl_idname = "OBJECT_PT_outline_rendering"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Shaders"
+	bl_label = "Outline Rendering"
+	bl_idname = "OBJECT_PT_outline_rendering"
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'UI'
+	bl_category = "Shaders"
 
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-        layout.prop(scene, "use_outline_shaders")
-        layout.prop(scene, "custom_outline_size")
+	def draw(self, context):
+		layout = self.layout
+		scene = context.scene
+		layout.prop(scene, "use_outline_shaders")
+		layout.prop(scene, "custom_outline_size")
 
 def register():
-    bpy.utils.register_class(OBJECT_PT_outline_rendering)
-    bpy.types.Scene.custom_shaders_draw_handlers = []
-    bpy.types.Scene.use_outline_shaders = bpy.props.BoolProperty(name = "Use outline shaders")
-    bpy.types.Scene.custom_outline_size = bpy.props.FloatProperty(name = "Outline thickness")
+	bpy.utils.register_class(OBJECT_PT_outline_rendering)
+	bpy.types.Scene.custom_shaders_draw_handlers = []
+	bpy.types.Scene.use_outline_shaders = bpy.props.BoolProperty(name = "Use outline shaders")
+	bpy.types.Scene.custom_outline_size = bpy.props.FloatProperty(name = "Outline thickness")
 
 def unregister():
-    bpy.utils.unregister_class(OBJECT_PT_outline_rendering)
-    del bpy.types.Scene.use_custom_shaders
+	bpy.utils.unregister_class(OBJECT_PT_outline_rendering)
+	del bpy.types.Scene.use_custom_shaders
 
 if __name__ == "__main__":
-    register()
-    bpy.app.handlers.depsgraph_update_post.append(draw_mesh)
-    bpy.app.handlers.frame_change_post.append(draw_mesh)
+	register()
+	bpy.app.handlers.depsgraph_update_post.append(draw_mesh)
+	bpy.app.handlers.frame_change_post.append(draw_mesh)
--- vertex_colors.py
+++ vertex_colors.py
@@ -5,163 +5,163 @@ from mathutils import *
 from math import *
 
 CHANNEL_INDICES = {
-    'R': 0,
-    'G': 1,
-    'B': 2,
-    'A': 3
+	'R': 0,
+	'G': 1,
+	'B': 2,
+	'A': 3
 }
 
 def vertex_colors_from_group(object, vertex_color, vertex_group, color_channel, blend_mode):
-    data = object.data
-    for poly in data.polygons:
-        for loop_id in poly.loop_indices:
-            vertex = data.loops[loop_id].vertex_index
-            color = vertex_color.data[loop_id].color
-            weight = 0
-            try:
-                weight = vertex_group.weight(vertex)
-            except RuntimeError:
-                pass
-            if blend_mode == "REPLACE":
-                color[color_channel] = weight
-            elif blend_mode == "MULTIPLY":
-                color[color_channel] *= weight
-            data.vertex_colors.active.data[loop_id].color = color
+	data = object.data
+	for poly in data.polygons:
+		for loop_id in poly.loop_indices:
+			vertex = data.loops[loop_id].vertex_index
+			color = vertex_color.data[loop_id].color
+			weight = 0
+			try:
+				weight = vertex_group.weight(vertex)
+			except RuntimeError:
+				pass
+			if blend_mode == "REPLACE":
+				color[color_channel] = weight
+			elif blend_mode == "MULTIPLY":
+				color[color_channel] *= weight
+			data.vertex_colors.active.data[loop_id].color = color
 
 def fill_vertex_colors(object, vertex_color, fill_color, channels, blend_mode):
-    data = object.data
-    for poly in data.polygons:
-        for loop_id in poly.loop_indices:
-            color = vertex_color.data[loop_id].color
-            if blend_mode == "REPLACE":
-                for i in range(0, 3):
-                    color[i] = fill_color[i] if channels[i] else color[i]
-            elif blend_mode == "MULTIPLY":
-                for i in range(0, 3):
-                    color[i] *= fill_color[i] if channels[i] else color[i]
-            data.vertex_colors.active.data[loop_id].color = color
+	data = object.data
+	for poly in data.polygons:
+		for loop_id in poly.loop_indices:
+			color = vertex_color.data[loop_id].color
+			if blend_mode == "REPLACE":
+				for i in range(0, 3):
+					color[i] = fill_color[i] if channels[i] else color[i]
+			elif blend_mode == "MULTIPLY":
+				for i in range(0, 3):
+					color[i] *= fill_color[i] if channels[i] else color[i]
+			data.vertex_colors.active.data[loop_id].color = color
 
 class OBJECT_OT_write_vertex_colors(bpy.types.Operator):
-    bl_idname = "object.write_vertex_colors"
-    bl_label = "Copy weights to vertex colors"
-    bl_options = {"REGISTER", "UNDO"}
-    
-    src_group = bpy.props.StringProperty(name = "Vertex group")
-    dst_colors = bpy.props.StringProperty(name = "Vertex colors")
-    
-    channel: bpy.props.EnumProperty(
-        items = [
-            ("R", "Red", ""),
-            ("G", "Green", ""),
-            ("B", "Blue", ""),
-            ("A", "Alpha", "")
-        ],
-        name = "Color channel")
-    blend: bpy.props.EnumProperty(
-        items = [
-            ("REPLACE", "Replace", "", 0),
-            ("MULTIPLY", "Multiply", "", 1)
-        ],
-        name = "Blend mode")
-        
-    def invoke(self, context, event):
-        self.src_group = context.active_object.vertex_groups.active.name
-        self.dst_colors = context.active_object.data.vertex_colors.active.name
-        self.channel = "R"
-        self.blend = "REPLACE"
-        return context.window_manager.invoke_props_dialog(self)
-    
-    def draw(self, context):
-        layout = self.layout
-        layout.prop_search(self, "src_group", context.active_object, "vertex_groups")
-        layout.prop_search(self, "dst_colors", context.active_object.data, "vertex_colors")
-        layout.prop(self, "channel", expand = True)
-        layout.prop(self, "blend")
-    
-    def execute(self, context):
-        object = bpy.context.active_object
-        vertex_colors_from_group(
-            object,
-            object.data.vertex_colors[self.dst_colors],
-            object.vertex_groups[self.src_group],
-            CHANNEL_INDICES[self.channel], self.blend
-        )
-        return {"FINISHED"}
+	bl_idname = "object.write_vertex_colors"
+	bl_label = "Copy weights to vertex colors"
+	bl_options = {"REGISTER", "UNDO"}
+
+	src_group = bpy.props.StringProperty(name = "Vertex group")
+	dst_colors = bpy.props.StringProperty(name = "Vertex colors")
+
+	channel: bpy.props.EnumProperty(
+		items = [
+			("R", "Red", ""),
+			("G", "Green", ""),
+			("B", "Blue", ""),
+			("A", "Alpha", "")
+		],
+		name = "Color channel")
+	blend: bpy.props.EnumProperty(
+		items = [
+			("REPLACE", "Replace", "", 0),
+			("MULTIPLY", "Multiply", "", 1)
+		],
+		name = "Blend mode")
+
+	def invoke(self, context, event):
+		self.src_group = context.active_object.vertex_groups.active.name
+		self.dst_colors = context.active_object.data.vertex_colors.active.name
+		self.channel = "R"
+		self.blend = "REPLACE"
+		return context.window_manager.invoke_props_dialog(self)
+
+	def draw(self, context):
+		layout = self.layout
+		layout.prop_search(self, "src_group", context.active_object, "vertex_groups")
+		layout.prop_search(self, "dst_colors", context.active_object.data, "vertex_colors")
+		layout.prop(self, "channel", expand = True)
+		layout.prop(self, "blend")
+
+	def execute(self, context):
+		object = bpy.context.active_object
+		vertex_colors_from_group(
+			object,
+			object.data.vertex_colors[self.dst_colors],
+			object.vertex_groups[self.src_group],
+			CHANNEL_INDICES[self.channel], self.blend
+		)
+		return {"FINISHED"}
 
 class OBJECT_OT_fill_vertex_colors(bpy.types.Operator):
-    bl_idname = "object.fill_vertex_colors"
-    bl_label = "Fill vertex colors"
-    bl_options = {"REGISTER", "UNDO"}
-    
-    fill_color: bpy.props.FloatVectorProperty(
-        name = "Color",
-        subtype = "COLOR",
-        size = 4,
-        min = 0,
-        max = 1)
-    
-    channels: bpy.props.BoolVectorProperty(name = "Channels", size = 4, subtype="COLOR")
-    
-    blend: bpy.props.EnumProperty(
-        items = [
-            ("REPLACE", "Replace", "", 0),
-            ("MULTIPLY", "Multiply", "", 1)
-        ],
-        name = "Blend mode")
-    
-    def invoke(self, context, event):
-        self.fill_color = (1, 1, 1, 1)
-        self.channels = (True, True, True, True)
-        self.blend = "REPLACE"
-        return context.window_manager.invoke_props_dialog(self)
-    
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, "fill_color")
-        layout.prop(self, "channels", expand = True)
-        layout.prop(self, "blend")
-    
-    def execute(self, context):
-        object = bpy.context.active_object
-        fill_vertex_colors(
-            object,
-            object.data.vertex_colors.active,
-            self.fill_color,
-            self.channels,
-            self.blend,
-        )
-        return {"FINISHED"}
+	bl_idname = "object.fill_vertex_colors"
+	bl_label = "Fill vertex colors"
+	bl_options = {"REGISTER", "UNDO"}
+
+	fill_color: bpy.props.FloatVectorProperty(
+		name = "Color",
+		subtype = "COLOR",
+		size = 4,
+		min = 0,
+		max = 1)
+
+	channels: bpy.props.BoolVectorProperty(name = "Channels", size = 4, subtype="COLOR")
+
+	blend: bpy.props.EnumProperty(
+		items = [
+			("REPLACE", "Replace", "", 0),
+			("MULTIPLY", "Multiply", "", 1)
+		],
+		name = "Blend mode")
+
+	def invoke(self, context, event):
+		self.fill_color = (1, 1, 1, 1)
+		self.channels = (True, True, True, True)
+		self.blend = "REPLACE"
+		return context.window_manager.invoke_props_dialog(self)
+
+	def draw(self, context):
+		layout = self.layout
+		layout.prop(self, "fill_color")
+		layout.prop(self, "channels", expand = True)
+		layout.prop(self, "blend")
+
+	def execute(self, context):
+		object = bpy.context.active_object
+		fill_vertex_colors(
+			object,
+			object.data.vertex_colors.active,
+			self.fill_color,
+			self.channels,
+			self.blend,
+		)
+		return {"FINISHED"}
 
 class OBJECT_PT_vertex_colors(bpy.types.Panel):
-    bl_label = "Vertex Colors"
-    bl_idname = "OBJECT_PT_vertex_colors"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Vertex Data"
-    
-    @classmethod
-    def poll(self, context):
-        return not isinstance(context.active_object, type(None)) and context.active_object.type == "MESH"
+	bl_label = "Vertex Colors"
+	bl_idname = "OBJECT_PT_vertex_colors"
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'UI'
+	bl_category = "Vertex Data"
 
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-        layout.operator("object.write_vertex_colors")
-        layout.operator("object.fill_vertex_colors")
+	@classmethod
+	def poll(self, context):
+		return not isinstance(context.active_object, type(None)) and context.active_object.type == "MESH"
+
+	def draw(self, context):
+		layout = self.layout
+		scene = context.scene
+		layout.operator("object.write_vertex_colors")
+		layout.operator("object.fill_vertex_colors")
 
 classes = [
-    OBJECT_OT_fill_vertex_colors,
-    OBJECT_OT_write_vertex_colors,
-    OBJECT_PT_vertex_colors,
+	OBJECT_OT_fill_vertex_colors,
+	OBJECT_OT_write_vertex_colors,
+	OBJECT_PT_vertex_colors,
 ]
 
 def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
+	for cls in classes:
+		bpy.utils.register_class(cls)
 
 def unregister():
-    for cls in classes:
-        bpy.utils.register_class(cls)
+	for cls in classes:
+		bpy.utils.register_class(cls)
 
 if __name__ == "__main__":
-    register()
+	register()
